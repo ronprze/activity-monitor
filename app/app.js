d473b23aba722499d2c7a8bbc32da50c74eeafc0
ronprze <perez.ron@protonmail.com>
2018-07-25 12:02:50 +0300

    Updating routing app.js

--- app/app.js
+++ app/app.js
@@ -1,1 +1,24 @@
-var app = angular.module('activity-monitor', []);
+var app = angular.module('activity-monitor', ['ngRoute']);
+
+app.config(function($routeProvider){
+    $routeProvider
+    .when("/", {
+        templateUrl: "home.html"
+    })
+    .when("/login", {
+        templateUrl: "login.html",
+        controller: "loginCtrl"
+    })
+    .when("/signup", {
+
+    })
+    .when("profile/:profileId", {
+
+    })
+    .when("workout/:workoutId", {
+        
+    })
+    .otherwise({
+        redirectTo:"/"
+    })
+});
