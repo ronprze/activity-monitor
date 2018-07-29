83adc03403d9956f4540161bd707c3d430a7cd5c
ronprze <perez.ron@protonmail.com>
2018-07-29 11:31:08 +0300

    loginCtrl initial login function

---
+++ app/login/loginCtrl.js
@@ -0,0 +1,14 @@
+app.controller('loginCtrl', function($scope, user, $location){
+    $scope.email = "";
+    $scope.username = "";
+    $scope.password = "";
+    $scope.invalidLogin = false;
+
+    $scope.login = function(){
+        user.login($scope.email, $scope.password).then(function(activeUser){
+            $location.path("/");
+        }, function(){
+            $scope.invalidLogin = true;
+        })
+    }
+});
