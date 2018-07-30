d52789708be19671ab26a9fcb51430b894a1f267
ronprze <perez.ron@protonmail.com>
2018-07-30 10:18:03 +0300

    fixed login() promise

--- app/login/loginCtrl.js
+++ app/login/loginCtrl.js
@@ -1,11 +1,11 @@
-app.controller('loginCtrl', function($scope, user, $location){
+app.controller('loginCtrl', function($scope, usr, $location){
     $scope.email = "";
-    //$scope.username = "";
     $scope.password = "";
     $scope.invalidLogin = false;
 
     $scope.login = function(){
-        user.login($scope.email, $scope.password).then(function(activeUser){
+        $scope.invalidLogin = false;
+        usr.login($scope.email, $scope.password).then(function(activeUser){
             $location.path("app/profile/:profileId/dashboard.html");
         }, function(){
             $scope.invalidLogin = true;
--- app/model/userSrv.js
+++ app/model/userSrv.js
@@ -1,5 +1,5 @@
-app.factory('user', function($http, $q){
-    var activeUser = {};
+app.factory('usr', function($http, $q){
+    var activeUser = null; //new User({fname:"ron", lname:"pe", email:"r@r.com", id:1});
 
     function User(plainUser){
         this.fname = plainUser.fname;
@@ -11,26 +11,29 @@ app.factory('user', function($http, $q){
     function login(email, password){
         var async = $q.defer();
 
-        var loginURL = "https://my-json-server.typicode.com/ronprze/activity-monitor/users/1"; // + email + "&password=" + password
+        var loginURL = "https://my-json-server.typicode.com/ronprze/activity-monitor/users"; // + email + "&password=" + password
         $http.get(loginURL).then(function(response){
-            if (response.data.email === email && response.data.password === password){
-                activeUser = new User(response.data);
-                async.resolve(activeUser);
-            } else {
-                async.reject("Invalid User credentials");
+            for (var i = 0; i < response.data.length; i++){
+                if (response.data[i].email === email && response.data[i].password === password){
+                    activeUser = new User(response.data[i]);
+                    async.resolve(activeUser);
+                    break;
+                } else {
+                    async.reject("Invalid User credentials");
+                }
             }
         }, function(err){
             async.reject(err);
         });
 
-        return async.promis;
+        return async.promise;
     }
 
     function isUserLoggedIn(){
         return activeUser ? true : false;
     }
 
-    function logOut(){
+    function logout(){
         activeUser = null;
     }
 
@@ -40,7 +43,7 @@ app.factory('user', function($http, $q){
 
     return {
         login : login,
-        logOut : logOut,
+        logout : logout,
         isUserLoggedIn : isUserLoggedIn,
         getActiveUser : getActiveUser
     }
