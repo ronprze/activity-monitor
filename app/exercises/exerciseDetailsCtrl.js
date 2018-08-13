7e4b7bd0fc9373fa963170115dcb3f08c352f6f1
ronprze <perez.ron@protonmail.com>
2018-08-13 14:02:22 +0300

    ronprze

--- app/exercises/exerciseDetailsCtrl.js
+++ app/exercises/exerciseDetailsCtrl.js
@@ -10,6 +10,9 @@ app.controller('exerciseDetailsCtrl',function($scope, $location, exercises, usr,
     exercises.getExerciseById($routeParams.id).then(function(response){
         $scope.exercise = response[0];
         $scope.videoUrl = $sce.trustAsResourceUrl($scope.exercise.details.videoUrl);
+        $scope.isVideoUrl = function(){
+            return ($scope.exercise.details.videoUrl !== "") ? true:false;
+        }
     });
-    
+
 });
